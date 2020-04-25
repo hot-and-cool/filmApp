049a147a13b899a4cdee547a94de5c7a0c7536df
EC2 Default User <ec2-user@ip-172-31-94-183.ec2.internal>
2020-04-25 09:12:49 +0000

    詳細画面にキャスト一覧表示

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -11,17 +11,17 @@
       <section class='review-main'>
         <div class='base-info'>
           <p class='h2 title'>{{ $item['title'] }}<span class='small pl10'>({{ $release_year }}公開)</span> </p>
-          <p class='mb10 original-title text-muted'>{{ $item['original_title']}}</p>
-          <p>公開日:{{ $release_date }} / 制作:{{$item['production_countries'][0]['name']}} / 上映時間:{{ $item['runtime'] }}分</p>
-          <p class='release-date'>ジャンル:
+          <p class='pb10 original-title text-muted'>{{ $item['original_title']}}</p>
+          <p class='pb10'>公開日:{{ $release_date }} / 制作:{{$item['production_countries'][0]['name']}} / 上映時間:{{ $item['runtime'] }}分</p>
+          <p class='release-date pb10'>ジャンル:
               @foreach ($item['genres'] as $genre)
-                <span class="text-primary">{{ $genre['name'] }}</span>
+                <a href="#" class="text-primary">{{ $genre['name'] }}</a>
                 <span>/</span>
               @endforeach
           </p>
           <p id='star' class=''>tmdb評価({{ $item['vote_count'] }})<span class='h3 pl10 text-yellow'>★{{ $item['vote_average'] }}</span></p>
         </div>
-        <div class="over-view">
+        <div class='over-view'>
           <h3>あらすじ</h3>
           @if (!empty($item['overview']))
           <p>{{ $item['overview'] }}</p>
@@ -29,7 +29,12 @@
           <p>作品のあらすじは秘密です。</p>
           @endif
         </div>
-        <a href="{{ route('credit', ['id' => $id, 'search-movie' => $searchWord]) }}" class='btn btn-secondary'>キャスト一覧</a>
+        <div class='casts pt30'>
+          <p>出演者</p>
+        @foreach ($casts as $cast)
+          <div class='btn btn-secondary mb10'>{{ $cast['name'] }}</div>
+        @endforeach
+        </div>
       </section>  
       <aside class='review-image pl10'>
       @empty($item['poster_path'])
@@ -39,11 +44,11 @@
       @endempty
       </aside>
     </div>
-    <div class="card-body similar-movies">
+    <div class="card-body recommend-movies">
       <h3>関連作品</h3>
-    @foreach ($similarMovies as $similarMovie)
+    @foreach ($recommendMovies as $recommendMovie)
       <div class="small-wrapper">
-        <img class="image-size" src="https://image.tmdb.org/t/p/w200{{ $similarMovie['poster_path'] }}"></img>
+        <a href="{{ route('show',['id' => $recommendMovie['id']]) }}"><img class="image-size" src="https://image.tmdb.org/t/p/w200{{ $recommendMovie['poster_path'] }}"></img></a>
       </div>
     @endforeach
     </div>
