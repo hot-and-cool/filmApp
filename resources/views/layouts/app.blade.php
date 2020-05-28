8bd392f98a763656821780fe5fec33707ca89d43
hot-and-cool <hisato0217take@gmail.com>
2020-05-28 16:08:37 +0900

    ファビコンの設定

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,8 +7,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <title>{{ config('app.name', 'Films') }}</title>
+    <link rel="shortcut icon" href="{{ asset('/films-logo-w-s.ico') }}" type="image/x-icon">
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
